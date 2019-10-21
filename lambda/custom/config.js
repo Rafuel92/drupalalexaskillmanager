cee00a8c66303dd96e6593a6e580dbda09d9e2e0
Raffaele Chiocca <raffaele.chiocca@gmail.com>
2019-10-21 14:47:34 +0200

    Initial commit

---
+++ lambda/custom/config.js
@@ -0,0 +1,9 @@
+const DRUPAL_HOST_NAME = '%hostnameplaceholder%';
+const DRUPAL_PROTOCOL = '%protocolplaceholder%';
+
+const config = {
+    "hostname" : DRUPAL_HOST_NAME,
+    "protocol" : DRUPAL_PROTOCOL
+};
+
+module.exports = config;
