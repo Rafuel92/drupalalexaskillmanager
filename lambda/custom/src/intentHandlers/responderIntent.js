d39f9cb244178d4ad9e072be418d65c14468495b
Raffaele Chiocca <raffaele.chiocca@gmail.com>
2019-12-12 15:24:06 +0100

    Add authentication management
    Add authentication management

--- lambda/custom/src/intentHandlers/responderIntent.js
+++ lambda/custom/src/intentHandlers/responderIntent.js
@@ -2,21 +2,46 @@ const Alexa = require('ask-sdk-core');
 const config = require('../../config');
 const httpGet = require('../common/httpGet');
 const httpPost = require('../common/httpPost');
+const authenticatedIntents = ['%placeholder%'];
 
-const ResponderIntent = {
+ResponderIntent = {
     canHandle(handlerInput) {
         return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest';
     },
     handle(handlerInput) {
         const receivedIntentData = handlerInput.requestEnvelope.request.intent;
-        return new Promise(function(resolve, reject) {
+        const receivedIntentName = handlerInput.requestEnvelope.request.intent.name;
+        let needsAuth = false;
+        if (authenticatedIntents.indexOf(receivedIntentName) > -1) {
+            needsAuth = true;
+        }
+        return new Promise(function (resolve, reject) {
             httpPost(config.hostname, JSON.stringify(receivedIntentData), (theResult) => {
                 console.log(theResult);
                 const parsed = JSON.parse(theResult);
-                resolve(handlerInput.responseBuilder
-                    .speak(parsed.output)
-                    .getResponse()
-                );
+                if(needsAuth) {
+                    let accessToken = handlerInput.requestEnvelope.context.System.user.accessToken;
+                    if(accessToken === undefined) {
+                        resolve(
+                            handlerInput.responseBuilder
+                                .speak("Please authenticate yourself")
+                                .withLinkAccountCard()
+                                .getResponse()
+                        );
+                    } else {
+                        resolve(
+                            handlerInput.responseBuilder
+                                .speak(parsed.output)
+                                .getResponse()
+                        );
+                    }
+                }  else {
+                    resolve(
+                        handlerInput.responseBuilder
+                            .speak(parsed.output)
+                            .getResponse()
+                    );
+                }
             });
         });
     }
